b7f029e8171cbe67e325ab1aee173e14768008bf
h1013iris <h1013iris@gmail.com>
2022-03-20 16:49:10 +0900

    20220320_commit

--- jspServer/src/main/java/com/uni/serviceCenter/model/service/ServiceCenterService.java
+++ jspServer/src/main/java/com/uni/serviceCenter/model/service/ServiceCenterService.java
@@ -1,6 +1,9 @@
 package com.uni.serviceCenter.model.service;
 
-import static com.uni.common.JDBCTemplate.*;
+import static com.uni.common.JDBCTemplate.close;
+import static com.uni.common.JDBCTemplate.commit;
+import static com.uni.common.JDBCTemplate.getConnection;
+import static com.uni.common.JDBCTemplate.rollback;
 
 import java.sql.Connection;
 import java.util.ArrayList;
@@ -26,5 +29,33 @@ public class ServiceCenterService {
 		
 		return list;
 	}
+
+
+	public ArrayList<ServiceCenter> findCList(String selectinput, String input) {
+		Connection conn = getConnection();
+		
+		ArrayList<ServiceCenter> list = new ServiceCenterDao().findCList(conn, selectinput, input);
+		close(conn);
+		
+		return list;
+	}
+
+
+	public int insertServiceCenter(ServiceCenter sc) {//고객센터 글 등록
+		Connection conn = getConnection();
+		
+		int result = new ServiceCenterDao().insertServiceCenter(conn,sc);
+		
+		if(result > 0) {
+			commit(conn);
+		}else {
+			rollback(conn);
+		}
+		close(conn);
+		return result;
+	}
+
+
+	
 
 }
